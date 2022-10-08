eaab4bad713ff4dc1525c794849aaff58f632c07
xx025 <71559822+xx025@users.noreply.github.com>
2022-10-08 20:41:04 +0800

    Update test_img.py

--- practice1/test_img.py
+++ practice1/test_img.py
@@ -1,9 +1,5 @@
 import unittest
 
-import cv2
-
-from practice1.pic_ import IMGU
-
 
 class MyTestCase(unittest.TestCase):
 
@@ -15,7 +11,10 @@ class MyTestCase(unittest.TestCase):
         灰度图像的直方图计算和显示
         :return:
         """
-        img = cv2.imread('../practice1/imgs/img21.png', cv2.IMREAD_GRAYSCALE)
+        import cv2
+        from practice1.pic_ import IMGU
+
+        img = cv2.imread('../opencv-samples-data/left01.jpg', cv2.IMREAD_GRAYSCALE)
 
         IMGU.zft(img, draw=True)
         cv2.imshow('img', img)
@@ -23,28 +22,41 @@ class MyTestCase(unittest.TestCase):
 
     def test_f2(self):
         # 灰度图像的负像，
-        img = cv2.imread('../practice1/imgs/img21.png', cv2.IMREAD_GRAYSCALE)
+        import cv2
+        from practice1.pic_ import IMGU
+
+        img = cv2.imread('../opencv-samples-data/left01.jpg', cv2.IMREAD_GRAYSCALE)
         a4 = IMGU.fx(img)
         cv2.imshow('res', a4)
         cv2.waitKey()
 
     def test_f3(self):
         # 灰度级转换
-        img = cv2.imread('../practice1/imgs/img21.png', cv2.IMREAD_GRAYSCALE)
+
+        import cv2
+        from practice1.pic_ import IMGU
+
+        img = cv2.imread('../opencv-samples-data/left01.jpg', cv2.IMREAD_GRAYSCALE)
         a4 = IMGU.covert_to_16(img)
         cv2.imshow('res', a4)
         cv2.waitKey()
 
     def test_f4(self):
         # 生成噪声图
-        img = cv2.imread('../practice1/imgs/img21.png', cv2.IMREAD_GRAYSCALE)
+        import cv2
+        from practice1.pic_ import IMGU
+
+        img = cv2.imread('../opencv-samples-data/left01.jpg', cv2.IMREAD_GRAYSCALE)
         a4 = IMGU.noise(img, sigma=20)
         cv2.imshow('res', a4[1])
         cv2.waitKey()
 
     def test_f5(self):
         # 图像相加去噪
-        img = cv2.imread('../practice1/imgs/img21.png', cv2.IMREAD_GRAYSCALE)
+        import cv2
+        from practice1.pic_ import IMGU
+
+        img = cv2.imread('../opencv-samples-data/left01.jpg', cv2.IMREAD_GRAYSCALE)
         imgs = IMGU.noise(img, sigma=20)
         img = IMGU.mean(imgs)
         cv2.imshow('res', img)
