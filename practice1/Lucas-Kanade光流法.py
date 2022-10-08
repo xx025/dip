677960c3a4eff48f7e0de17c12b0814ab971698c
xx025 <71559822+xx025@users.noreply.github.com>
2022-10-08 20:16:26 +0800

    Create Lucas-Kanade光流法.py

---
+++ practice1/Lucas-Kanade光流法.py
@@ -0,0 +1,63 @@
+"""
+来源：https://opencv.apachecn.org/#/docs/4.0.0/6.2-tutorial_py_lucas_kanade
+"""
+import cv2 as cv
+import numpy as np
+
+cap = cv.VideoCapture('../opencv-4.6.0/samples/data/vtest.avi')
+
+# ShiTomasi 角点检测的参数
+feature_params = dict(maxCorners=100,
+                      qualityLevel=0.3,
+                      minDistance=7,
+                      blockSize=7)
+
+# Lucas-Kanade 光流算法的参数
+lk_params = dict(winSize=(15, 15),
+                 maxLevel=2,
+                 criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
+
+# 创建一组随机颜色数
+color = np.random.randint(0, 255, (100, 3))
+
+# 取第一帧并寻找角点
+ret, old_frame = cap.read()
+old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
+p0 = cv.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
+
+# 创建绘制轨迹用的遮罩图层
+mask = np.zeros_like(old_frame)
+
+while (1):
+    ret, frame = cap.read()
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # 计算光流
+    p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+
+    # 选取最佳始末点
+    good_new = p1[st == 1]
+    good_old = p0[st == 1]
+
+    # 绘制轨迹
+    for i, (new, old) in enumerate(zip(good_new, good_old)):
+        a, b = new.ravel()
+        c, d = old.ravel()
+        a, b = int(a), int(b)       # 改动
+        c, d = int(c), int(d)        # 改动
+
+        mask = cv.line(mask, (a, b), (c, d), color[i].tolist(), 2)
+        frame = cv.circle(frame, (a, b), 5, color[i].tolist(), -1)
+    img = cv.add(frame, mask)
+
+    cv.imshow('frame', img)
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+    # 更新选取帧与特征点
+    old_gray = frame_gray.copy()
+    p0 = good_new.reshape(-1, 1, 2)
+
+cv.destroyAllWindows()
+cap.release()
