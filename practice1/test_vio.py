426fb2868b5ba52667fa6d93226d38f95c217bdb
xx025 <71559822+xx025@users.noreply.github.com>
2022-10-08 20:16:39 +0800

    Create test_vio.py

---
+++ practice1/test_vio.py
@@ -0,0 +1,61 @@
+import unittest
+
+import cv2
+
+from practice1.vio_ import BackSub
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_frames_diff(self):
+        """
+        测试帧差法
+        """
+
+        from practice1.vio_ import Video
+        from practice1.vio_ import FrameDiff
+
+        vid = Video(path='../practice1/video/Laboratory_raw.avi')
+
+        print(len(vid.frames))
+
+        l1 = vid.frames[106]
+        l2 = vid.frames[107]
+
+        op = FrameDiff(l1=l1, l2=l2)
+
+        lt = op.frame_diff()
+        rel = op.rec_angl(diff=lt)
+
+        cv2.imshow('diff', rel)
+
+        cv2.waitKey()
+
+    def test_bs(self):
+        """
+        测试背景拆分- 背景生成
+        :return:
+        """
+        from practice1.vio_ import Video
+
+        vid = Video(path='../practice1/video/Laboratory_raw.avi')
+
+        vid_bs = BackSub(vid=vid)
+
+        bgk = vid_bs.build_back_ground()
+
+        # Pickle any object to file
+        D = bgk
+
+        F = open('datafile.pkl', 'wb')
+        import pickle
+        pickle.dump(D, F)
+        F.close()
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
