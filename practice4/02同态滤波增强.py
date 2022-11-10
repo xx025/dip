1169af3e079d34ef680f826c11d1c35105045da0
xx025 <71559822+xx025@users.noreply.github.com>
2022-11-10 15:29:52 +0800

    Create 02同态滤波增强.py

---
+++ practice4/02同态滤波增强.py
@@ -0,0 +1,17 @@
+"""
+同态滤波增强
+"""
+import cv2
+
+from practice4.p0402 import homomorphic_filter
+from pylot_imgshow import img_show
+
+img = cv2.imread(r'p0402/data/lena_light.jpg', 0)
+# 读取图片
+
+
+filter_img = homomorphic_filter(img, r1=0.2, rh=1.5)
+# 将图片执行同态滤波器
+
+img_show(('Original image', img),
+         ("Filter image", filter_img))
